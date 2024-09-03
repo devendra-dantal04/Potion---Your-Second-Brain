ddb691984756d490f31a9d5e26eeeb1d49994680
Devendra Dantal <102410355+devendra-dantal04@users.noreply.github.com>
2024-09-03 21:45:39 +0530

    Some Bug Fixes

--- app/(marketing)/layout.tsx
+++ app/(marketing)/layout.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import Navbar from "./_components/Navbar";
 
-const MarketingLayout = (
-    { children }
-): { children: React.ReactNode } => {
+interface MarketingLayoutProps {
+  children: React.ReactNode;
+}
+
+const MarketingLayout = ({ children }: MarketingLayoutProps) => {
     return <div className="h-full dark:bg-[#1F1F1F]">
         <Navbar />
         <main className="h-full pt-40">
